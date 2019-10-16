e5f73a3f20e8eff177ddf43177b2d64fe6e6d56a
Martin Moldsvor <martin@moldsvor.com>
2019-10-16 14:24:51 +0200

    Merged Processing Unit with project

---
+++ Processing/__main__.py
@@ -0,0 +1,19 @@
+from .node import Node
+from .vector2D import Vector2D
+from .processing_unit import ProcessingUnit
+
+
+if __name__ == '__main__':
+    from .analysis import AnalysisTool
+
+    nodes = [Node(Vector2D(410, 90), 'COM4', 100), Node(Vector2D(90, 90), 'COM9', 100)]
+    processing_unit = ProcessingUnit(nodes, (100, 100, 400, 400))
+
+    while True:
+        analysisTool = AnalysisTool()
+        analysisTool.update_display(processing_unit.tracking_area, nodes)
+
+        position = processing_unit.position
+        if position is not None:
+            analysisTool.draw_point(position)
+        analysisTool.update()
